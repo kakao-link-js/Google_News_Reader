59b3d3a15cb403aa5d3fa0c7979b60dba8332e05
jaesung <cjs6027@naver.com>
2020-03-18 19:51:23 +0900

    init Project

---
+++ Project/app/src/main/java/com/kotlin/jaesungchi/rss_news_reader/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.jaesungchi.rss_news_reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Rss_Reader
-Rss_Reader
+![imge](https://img.shields.io/badge/ProjectType-SingleStudy-green) ![imge](https://img.shields.io/badge/Language-Kotlin-yellow) ![imge](https://img.shields.io/badge/Tools-AndroidStudio-blue)
+
+코틀린을 이용해 Rss 뉴스 리더를 만들어보는 프로젝트 입니다.
+
